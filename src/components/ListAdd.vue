8ef89300d9ae2f77e180cbee612dbf92ee3393ce
uni51 <unieye51@gmail.com>
2021-03-18 17:45:18 +0900

    3-2：ユーザビリティなフォームにしよう

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,14 +1,18 @@
 <template>
  <!-- サブミットイベント時にaddListメソッドをハンドルしている。 -->
  <!-- .preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぐ。 -->
- <form class="addlist" @submit.prevent="addList">
+ <form :class="classList" @submit.prevent="addList">
     <!-- v-modelはv-onとv-bindの2つの機能を持ち合わせている -->
     <input v-model="title"
            type="text"
            class="text-input"
            placeholder="Add new list"
+           @focusin="startEditing"
+           @focusout="finishEditing"
     >
-    <button type="submit" class="add-button">
+    <button type="submit"
+      class="add-button"
+      v-if="isEditing || titleExists">
       Add
     </button>
   </form>
@@ -19,13 +23,35 @@ export default {
   data: function() {
     return {
       title: '',
+      isEditing: false,
     }
   },
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      if (this.titleExists) {
+        classList.push('addable')
+      }      
+      return classList
+    },
+    titleExists() {
+      return this.title.length > 0
+    },    
+  },  
   methods: {
     addList: function() {
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
     },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
+    },    
   }  
 }
 </script>

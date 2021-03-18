0aaf5088fd5ab6ec464f06d59916a49785f0393b
uni51 <unieye51@gmail.com>
2021-03-18 17:28:27 +0900

    3-2 ユーザビリティなフォームにしよう

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,31 @@
+<template>
+ <!-- サブミットイベント時にaddListメソッドをハンドルしている。 -->
+ <!-- .preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぐ。 -->
+ <form class="addlist" @submit.prevent="addList">
+    <!-- v-modelはv-onとv-bindの2つの機能を持ち合わせている -->
+    <input v-model="title"
+           type="text"
+           class="text-input"
+           placeholder="Add new list"
+    >
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: '',
+    }
+  },
+  methods: {
+    addList: function() {
+      this.$store.dispatch('addlist', { title: this.title })
+      this.title = ''
+    },
+  }  
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,51 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem('trello-lists')
+
+const store = new Vuex.Store({
   state: {
+    // localStorageに保存されたリストがあれば取得、なければデフォルトのリスト配列を設置します。
+    lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English' },
+          { body: 'Mathematics' },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
+    addlist(state, payload) {
+      // Stateを更新する
+      state.lists.push({ title: payload.title, cards:[] })
+    },    
   },
   actions: {
+    addlist(context, payload) {
+      // MutationsをCommit（実行）する
+      context.commit('addlist', payload)
+    },   
   },
-  modules: {
+  getters: {
   }
 })
+
+// データの状態を更新後にlocalStorageへデータの状態を保存しています
+// subscribeはストアのインスタンスメソッドで、全てのmutationの後に呼ばれます。
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store

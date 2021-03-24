7359df527db3424e57d86187c55858c2c0ce9435
uni51 <unieye51@gmail.com>
2021-03-24 17:32:00 +0900

    3-3: Listコンポーネントを作成しよう

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,17 +5,33 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :listIndex="index"
+        />      
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import ListAdd from './ListAdd.vue'
+import List from './List'
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    ListAdd
-  },  
+    ListAdd,
+    List
+  },
+  computed: {
+    // stateで定義されたデータの名前と同じ名前の文字列でstateを呼び出すことができる
+    ...mapState([
+      'lists'
+    ]),
+  },    
 }
 </script>

b5110f2ed7d60686c275014a303764dfca9b9351
uni51 <unieye51@gmail.com>
2021-03-27 15:42:34 +0900

    5-3: Listのドラッグ&ドロップ機能を実装しよう

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,11 @@
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable 
+        :list="lists"
+        @end="movingList"
+        class="list-index"
+      >
         <list v-for="(item, index) in lists"
               :key="item.id"
               :title="item.title"
@@ -14,18 +18,20 @@
               @change="movingCard"
         />      
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
 
 export default {
   components: {
+    draggable,
     ListAdd,
     List
   },
@@ -42,6 +48,9 @@ export default {
     movingCard: function() {
       this.$store.dispatch('updateList', { lists: this.lists })
     },
+    movingList: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    }    
   }      
 }
 </script>

b8097b1c64ccb79cbcf00b1b9395da349956f16b
uni51 <unieye51@gmail.com>
2021-03-27 15:37:25 +0900

    5-2: Cardのドラッグ&ドロップ機能を実装しよう

--- src/store/index.js
+++ src/store/index.js
@@ -41,7 +41,10 @@ const store = new Vuex.Store({
     },
     removeCardFromList(state, payload) {
       state.lists[payload.listIndex].cards.splice(payload.cardIndex, 1)
-    }         
+    },
+    updateList(state, payload) {
+      state.lists = payload.lists
+    }             
   },
   actions: {
     addlist(context, payload) {
@@ -56,7 +59,10 @@ const store = new Vuex.Store({
     },
     removeCardFromList(context, payload) {
       context.commit('removeCardFromList', payload)    
-    },      
+    },
+    updateList(context, payload) {
+      context.commit('updateList', payload)
+    }         
   },
   getters: {
     totalCardCount(state) {
